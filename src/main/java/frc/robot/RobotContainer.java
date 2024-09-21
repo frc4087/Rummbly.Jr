9b38a240b20dc1146b2291d34fc25920e1482ef3
Elinor <ecreyher2008@gmail.com>
2024-09-21 12:38:44 -0500

    X

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,15 +16,19 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.FrankenArm;
 
 public class RobotContainer {
-
+  
   public LaserCan LaunchSensor = new LaserCan(TunerConstants.LaunchSensor);
   public LaserCan IntakeSensor = new LaserCan(TunerConstants.IntakeSensor);
   public TalonFX IntakeFeedMotor = new TalonFX(TunerConstants.IntakeFeed);
@@ -32,16 +36,20 @@ public class RobotContainer {
   public TalonFX LauncherFeedMotor = new TalonFX(TunerConstants.LaunchFeed);
   public TalonFX LaunchRtFlywheel = new TalonFX(TunerConstants.LaunchRtFlywheel);
   public TalonFX LaunchLtFlywheel = new TalonFX(TunerConstants.LaunchLtFlywheel);
+  public final CommandXboxController m_joystick = new CommandXboxController(1);
 
-  private final FrankenArm frankenArm = new FrankenArm();
+  Trigger xButton = m_joystick.x();
+  Trigger yButton = m_joystick.y();
+  Trigger aButton = m_joystick.a();
+  Trigger bButton = m_joystick.b();
 
   private double MaxSpeed = TunerConstants.kSpeedAt12VoltsMps; // kSpeedAt12VoltsMps desired top speed
   private double MaxAngularRate = 1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
 
   /* Setting up bindings for necessary control of the swerve drive platform */
   private final CommandXboxController joystick = new CommandXboxController(0); // My joystick
-  public final CommandXboxController m_joystick = new CommandXboxController(1);
-  private final CommandSwerveDrivetrain drivetrain = TunerConstants.DriveTrain; // My drivetrain
+  private final CommandSwerveDrivetrain drivetrain = TunerConstants.DriveTrain;
+  private final FrankenArm frankenArm = new FrankenArm();
 
   private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
       .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
@@ -51,6 +59,10 @@ public class RobotContainer {
   private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
 
   private final Telemetry logger = new Telemetry(MaxSpeed);
+
+  public RobotContainer() {
+    configureBindings();
+  }
 
   private void configureBindings() {
     drivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
@@ -71,14 +83,34 @@ public class RobotContainer {
       drivetrain.seedFieldRelative(new Pose2d(new Translation2d(), Rotation2d.fromDegrees(90)));
     }
     drivetrain.registerTelemetry(logger::telemeterize);
+
+    m_joystick.a().onTrue(new RunCommand(() -> {
+      frankenArm.setArmPosition(FrankenArm.SETPOINT1);
+      frankenArm.runIntake();
+    }, frankenArm));
+
+    m_joystick.b().onTrue(new RunCommand(() -> frankenArm.setArmPosition(FrankenArm.SETPOINT2), frankenArm));
+
+    m_joystick.x().onTrue(new RunCommand(() -> frankenArm.setArmPosition(FrankenArm.SETPOINT3), frankenArm));
+
+    m_joystick.y().onTrue(new RunCommand(() -> {
+      frankenArm.setArmPosition(FrankenArm.SETPOINT1);
+      frankenArm.runLauncher();
+    }, frankenArm));
+
   }
 
-  public RobotContainer() {
-    configureBindings();
+  public void teleoperiodic() {
+    LaserCan.Measurement measurement = IntakeSensor.getMeasurement();
+    double[] measurements = { measurement.getValue1(), measurement.getValue2() }; // Adjust based on your Measurement class
+    SmartDashboard.putNumberArray("ISM", measurements);
   }
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    // Define and return your autonomous command here
+    return null;
   }
+
+  
 
 }
---
+++ src/main/java/frc/robot/subsystems/FrankenArm.java
@@ -0,0 +1,242 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.robot.generated.TunerConstants;
+
+import au.grapplerobotics.LaserCan;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class FrankenArm extends SubsystemBase {
+  
+  public LaserCan LaunchSensor = new LaserCan(TunerConstants.LaunchSensor);
+  public LaserCan IntakeSensor = new LaserCan(TunerConstants.IntakeSensor);
+  public TalonFX IntakeFeedMotor = new TalonFX(TunerConstants.IntakeFeed);
+  public TalonFX IntakeCenterMotor = new TalonFX(TunerConstants.IntakeCenter);
+  public TalonFX LauncherFeedMotor = new TalonFX(TunerConstants.LaunchFeed);
+  public TalonFX LaunchRtFlywheel = new TalonFX(TunerConstants.LaunchRtFlywheel);
+  public TalonFX LaunchLtFlywheel = new TalonFX(TunerConstants.LaunchLtFlywheel);
+  public final TalonFX armMotor = new TalonFX(TunerConstants.ArmAngle);
+  public final CANcoder armSensor = new CANcoder(TunerConstants.ArmSensor);
+  public final MotionMagicVoltage motionMagic = new MotionMagicVoltage(0);
+
+  public static final double TOLERANCE = 3.0;
+  public static final double SETPOINT1 = 5;
+  public static final double SETPOINT2 = 10;
+  public static final double SETPOINT3 = 20;  
+  public boolean isChecking = false;
+
+  public FrankenArm() {
+    TalonFXConfiguration armMotorConfig = new TalonFXConfiguration();
+    
+    // Create MotionMagicConfigs object
+    MotionMagicConfigs motionMagicConfigs = new MotionMagicConfigs();
+    motionMagicConfigs.MotionMagicCruiseVelocity = 15000;
+    motionMagicConfigs.MotionMagicAcceleration = 6000;
+
+    // Apply MotionMagicConfigs to TalonFXConfiguration
+    armMotorConfig.MotionMagic = motionMagicConfigs;
+
+    // Create Slot0Configs object for PID configurations
+    Slot0Configs slot0Configs = new Slot0Configs();
+    slot0Configs.kP = 0.3;
+    slot0Configs.kI = 0.0;
+    slot0Configs.kD = 0.0001;
+
+    // Apply Slot0Configs to TalonFXConfiguration
+    armMotorConfig.Slot0 = slot0Configs;
+
+    StatusCode status = armMotor.getConfigurator().apply(armMotorConfig);
+
+    CANcoderConfiguration armSensorConfig = new CANcoderConfiguration();
+    status = armSensor.getConfigurator().apply(armSensorConfig);
+
+    // Set initial positions or states
+    armSensor.setPosition(0.0); // Set the position of the CANcoder sensor
+
+    // Set neutral mode
+    armMotor.setNeutralMode(NeutralModeValue.Brake);
+
+    initializeArm();
+  }
+
+  private void initializeArm() {
+    // Initial calibration
+    setArmPosition(SETPOINT1); // Move to a known position
+}
+
+public void setArmPosition(double position) {
+    armMotor.setControl(motionMagic.withPosition(position));
+  }
+
+public void runIntake() {
+    IntakeFeedMotor.getConfigurator().apply(new TalonFXConfiguration());
+    IntakeFeedMotor.setInverted(false);
+    IntakeFeedMotor.set(0.6);
+    IntakeCenterMotor.set(0.6);
+    shootOffIntake();
+    startChecking();
+    shootOffLaucher();
+} 
+
+public void runLauncher() {
+  LaunchRtFlywheel.set(1);
+  LaunchLtFlywheel.set(1);
+  LauncherFeedMotor.set(1);
+}
+
+public void shootOffIntake() {
+  LaserCan.Measurement measurement = IntakeSensor.getMeasurement();
+  if (measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT && measurement.distance_mm <= 300) {
+    IntakeFeedMotor.set(0);
+    IntakeCenterMotor.set(0);
+    LauncherFeedMotor.set(0);
+  }
+  }
+
+public void shootOffLaucher() {
+  LaserCan.Measurement measurement = IntakeSensor.getMeasurement();
+  if (measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT && measurement.distance_mm <= 300) {
+    LauncherFeedMotor.set(0);
+  }
+  }
+
+  public void startChecking() {
+    if (!isChecking) {
+        isChecking = true;
+        new Thread(() -> {
+            while (isChecking) {
+                double currentPosition = armSensor.getPosition().getValue(); // Extract the value from StatusSignal
+                if (Math.abs(currentPosition - SETPOINT1) <= TOLERANCE) {
+                    LauncherFeedMotor.set(0.6);
+                    IntakeCenterMotor.set(0.6);
+                    isChecking = false; // Stop checking once within tolerance
+                } else {
+                    LauncherFeedMotor.set(0);
+                }
+                try {
+                    Thread.sleep(100); // Sleep for a short period to avoid busy-waiting
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }).start();
+    }
+}
+}
+
+// public static final double FULL_SPEED = 0.65;
+// public static final double STOP = 0;
+// public static final double WARMUP = 0.5;
+
+// public final TalonFX m_fx = new TalonFX(TunerConstants.ArmAngle);
+  // public final CANcoder m_ex = new CANcoder(TunerConstants.ArmSensor);
+  //public final MotionMagicVoltage m_mmReq = new MotionMagicVoltage(0);
+
+  // public final double k_tolerance = 3.0;
+  // public boolean isChecking = false;
+  // public CommandXboxController m_joystick = new CommandXboxController(RobotContainer.m_joystick);
+  // double setpoint1 = 05;
+  // double setpoint2 = 10;
+  // double setpoint3 = 20;
+  // double fullSpeedAhead = 0.65;
+  // double Die = 0;
+  // double warmup = 0.5;
+
+//   public void shootOff(){
+  //   LaserCan.Measurement measurement = IntakeSensor.getMeasurement();
+  //   if (measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT && measurement.distance_mm <= 100) {
+  //     IntakeFeedMotor.set(Die);
+  //     IntakeCenterMotor.set(Die);
+  //     LauncherFeedMotor.set(Die);
+  //   }
+  // }
+  
+  // public void intakeOff(){
+  //   LaserCan.Measurement measurement = LaunchSensor.getMeasurement();
+  //   if (measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT && measurement.distance_mm <= 300) {
+  //     LauncherFeedMotor.set(Die);
+  //   }
+  // }
+
+// public void checkSet() {
+//     double currentPosition = m_ex.getPosition().getValue(); // Extract the value from StatusSignal
+//     if (Math.abs(currentPosition - setpoint1) <= k_tolerance) {
+//         LauncherFeedMotor.set(0.5);
+//     } else {
+//         LauncherFeedMotor.set(0);
+//     }
+//}
+
+// public void startChecking() {
+//     if (!isChecking) {
+//         isChecking = true;
+//         new Thread(() -> {
+//             while (isChecking) {
+//                 double currentPosition = m_ex.getPosition().getValue(); // Extract the value from StatusSignal
+//                 if (Math.abs(currentPosition - setpoint1) <= k_tolerance) {
+//                     LauncherFeedMotor.set(warmup);
+//                     isChecking = false; // Stop checking once within tolerance
+//                 } else {
+//                     LauncherFeedMotor.set(fullSpeedAhead);
+//                 }
+//                 try {
+//                     Thread.sleep(100); // Sleep for a short period to avoid busy-waiting
+//                 } catch (InterruptedException e) {
+//                     Thread.currentThread().interrupt();
+//                 }
+//             }
+//         }).start();
+//     }
+// }
+//}
+
+//public void initDefaultCommand() {
+      
+    //   m_joystick.a().onTrue(new RunCommand(() -> {
+    //   //m_fx.setControl(m_mmReq.withPosition(setpoint1));
+    //   IntakeFeedMotor.setInverted(true);
+    //   IntakeFeedMotor.set(1);
+    //   IntakeCenterMotor.set(1);
+    //   LauncherFeedMotor.set(1);
+    //   //shootOff();
+    //   //startChecking();
+    //   //intakeOff();
+    // }, this));
+
+    // m_joystick.b().onTrue(new RunCommand(() -> {
+    //   m_fx.setControl(m_mmReq.withPosition(setpoint2));
+    // }, this));
+
+    // m_joystick.x().onTrue(new RunCommand(() -> {
+    //   m_fx.setControl(m_mmReq.withPosition(setpoint3));
+    // }, this));
+
+    // m_joystick.y().onTrue(new RunCommand(() -> {
+      
+    //   m_fx.setControl(m_mmReq.withPosition(setpoint1));
+    //   LaunchRtFlywheel.set(1);
+    //   LaunchLtFlywheel.set(1);
+    //   LauncherFeedMotor.set(1);
+    // }, this));
